f377b61093c268bce546ea45cec2b50a991af8ed
John Yagiz <john.yagiz@gmail.com>
2021-10-13 20:02:19 -0700

    Particleground + Auodeploy

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import Spawn from './node_modules/@nurvus/spawn/index.js';
-const el = Spawn('You have spawned something!');
+// import particleground from './node_modules/particleground/jquery.particleground.min.js';
+// const el = Spawn('You have spawned something!');
+const el = Spawn({
+  className: 'particles',
+  style: {
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden'
+  }
+});
+
 document.body.appendChild(el);
+particleground(el);
